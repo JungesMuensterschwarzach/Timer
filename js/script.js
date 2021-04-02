ceb36df92ba4101ad496fe3a04139ae005f490d4
Lucas Kinne <developer.pad@outlook.com>
2021-04-03 01:01:20 +0200

    Add share button to generate link for timer state export/import Add link to app page via logo

--- js/script.js
+++ js/script.js
@@ -1,22 +1,67 @@
 class VideoTimer {
 
-    constructor(seconds, scene, playBackgroundMusic) {
-        this.currentSeconds = 0;
-        this.totalSeconds = seconds;
+    constructor(startUnixSeconds, endingUnixSeconds, text, scene, music) {
+        this.startUnixSeconds = startUnixSeconds;
+        this.currentUnixSeconds = Math.round(Date.now() / 1000);
+        this.endingUnixSeconds = endingUnixSeconds;
+        this.text = text;
         this.scene = scene;
-        this.playBackgroundMusic = playBackgroundMusic;
+        this.music = music;
+
+        if (this.currentUnixSeconds > this.endingUnixSeconds) {
+            this.currentUnixSeconds = this.endingUnixSeconds;
+        }
     }
 
     start() {
         $("#form").hide();
+        $("#autoplay").hide();
 
+        this.activateShareButton();
+        this.renderText();
         this.renderVideo();
-        if (this.playBackgroundMusic) {
+        if (this.music) {
             this.renderBackgroundMusic();
         }
 
+        $("body").css("overflow", "hidden");
         $("#player").show();
         this.beginTicking();
+    }
+
+    activateShareButton() {
+        const shareContainer = $("#share-container");
+        shareContainer.popover({
+            content: "In die Zwischenablage kopiert!",
+            title: "Link"
+        });
+        shareContainer.on("click", () => {
+            const textarea = $("<textarea/>").text(this.getShareableLink());
+            textarea.appendTo(document.body);
+            textarea.get(0).select();
+            textarea.get(0).setSelectionRange(0, 99999);
+            document.execCommand("copy");
+            textarea.remove();
+            setTimeout(() => {
+                shareContainer.popover("hide");
+            }, 2000);
+        });
+    }
+
+    getShareableLink() {
+        return window.location.href.split("?")[0] +
+            "?startUnixSeconds=" + encodeURIComponent(this.startUnixSeconds) +
+            "&endingUnixSeconds=" + encodeURIComponent(this.endingUnixSeconds) + 
+            "&text=" + encodeURIComponent(this.text) +
+            "&scene=" + encodeURIComponent(this.scene) +
+            "&music=" + encodeURIComponent(this.music);
+    }
+
+    renderText() {
+        if (this.text) {
+            $("#text").text(this.text);
+            $("#text-container").show();
+        }
     }
 
     renderVideo() {
@@ -38,9 +83,13 @@ class VideoTimer {
             // = onload
             const videoDOM = $("#video").get(0);
             const videoDuration = videoDOM.duration;
-            const timerDuration = this.totalSeconds;
+            const timerDuration = this.endingUnixSeconds - this.startUnixSeconds;
     
             videoDOM.playbackRate = videoDuration / timerDuration;
+            videoDOM.currentTime = 
+                (this.currentUnixSeconds - this.startUnixSeconds) /
+                (this.endingUnixSeconds - this.startUnixSeconds) *
+                videoDuration; 
             videoDOM.play();
         });
     }
@@ -68,6 +117,7 @@ class VideoTimer {
             type: "audio/mpeg"
         }).appendTo(audioObj);
         audioObj.prependTo($("#audio-container"));
+        audioObj.get(0).currentTime = this.currentUnixSeconds - this.startUnixSeconds;
         audioObj.get(0).volume = 0.5;
         audioObj.get(0).play();
     }
@@ -107,11 +157,11 @@ class VideoTimer {
 
     beginTicking() {
         this.intervalHandle = setInterval(() => {
-            this.currentSeconds++;
+            this.currentUnixSeconds++;
 
-            if (this.currentSeconds == Math.floor(this.totalSeconds / 2)) {
+            if (this.currentUnixSeconds == Math.floor(this.endingUnixSeconds / 2)) {
                 this.renderHalfTimeSound();
-            } else if (this.currentSeconds >= this.totalSeconds) {
+            } else if (this.currentUnixSeconds >= this.endingUnixSeconds) {
                 this.renderEndingTimeSound();
                 this.stop();
             }
@@ -125,22 +175,26 @@ class VideoTimer {
     }
 
     renderProgress() {
-        const remainingMinutes = Math.floor(this.getRemainingSeconds() / 60);
+        const remainingSeconds = this.endingUnixSeconds - this.currentUnixSeconds;
+        const remainingMinutes = Math.floor(remainingSeconds / 60);
 
         let text = "";
         if (remainingMinutes > 0) {
             text += remainingMinutes + " Minute" + (remainingMinutes !== 1 ? "n" : "");
+        } else if (remainingSeconds > 0) {
+            text += remainingSeconds + " Sekunde" + (remainingSeconds !== 1 ? "n" : "");
         } else {
-            const remainingSeconds = this.getRemainingSeconds();
-
-            if (remainingSeconds > 0) {
-                text += remainingSeconds + " Sekunde" + (remainingSeconds !== 1 ? "n" : "");
-            } else {
-                text += "Zeit abgelaufen";
-            }
+            text += "Zeit abgelaufen";
         }
 
-        $("#progressbar-progress").width(Math.ceil(this.currentSeconds * 100 / this.totalSeconds) + "%");
+        let progress = Math.ceil(
+            (this.currentUnixSeconds - this.startUnixSeconds) * 100 / 
+            (this.endingUnixSeconds - this.startUnixSeconds)
+        );
+        if (progress > 100) {
+            progress = 100;
+        }
+        $("#progressbar-progress").width(progress + "%");
         $("#progressbar-text").text(text);
     }
 
@@ -156,16 +210,42 @@ class VideoTimer {
         $("#progressbar-progress").addClass("finished");
     }
 
-    getRemainingSeconds() {
-        return this.totalSeconds - this.currentSeconds;
-    }
 }
 
-function start(form) {
+$(document).ready(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    if (!urlParams.get("startUnixSeconds")) {
+        return;
+    }
+
+    autoplay();
+});
+
+function manualStart(form) {
+    const currentUnixSeconds = Math.round(Date.now() / 1000);
     timer = new VideoTimer(
-        form.minutes.value * 60,
+        currentUnixSeconds,
+        currentUnixSeconds + form.minutes.value * 60,
+        form.text.value,
         form.scene.value,
         form.music.checked
+    );
+    timer.start();
+}
+
+function autoplay() {
+    $("#form").hide();
+    $("#autoplay").show();
+}
+
+function automaticStart() {
+    const urlParams = new URLSearchParams(window.location.search);
+    timer = new VideoTimer(
+        urlParams.get("startUnixSeconds"),
+        urlParams.get("endingUnixSeconds"),
+        urlParams.get("text"),
+        urlParams.get("scene"),
+        urlParams.get("music") === "true"
     );
     timer.start();
 }
